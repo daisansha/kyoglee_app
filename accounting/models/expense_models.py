c4ba0878b74fe52447595bc4068752b2a2c9a27b
daisansha <hujitenryutaase@gmail.com>
2025-06-08 13:42:14 +0900

    expense image

--- accounting/models/expense_models.py
+++ accounting/models/expense_models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from member.models import Member  # 団員モデルを参照
 from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
 class ExpenseApplication(models.Model):
     APPROVAL_CHOICES = [
@@ -15,9 +16,9 @@ class ExpenseApplication(models.Model):
     applicant = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='expenses')
     description = models.CharField(max_length=255)
     amount = models.IntegerField(verbose_name="金額（円）")
-    receipt_image = models.ImageField(upload_to='receipts/', blank=True, null=True)
+    #receipt_image = models.ImageField(upload_to='receipts/', blank=True, null=True)
+    receipt_image = CloudinaryField('画像', blank=True, null=True)
     submitted_at = models.DateTimeField(default=timezone.now)
-    #status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
     approval_status = models.CharField(max_length=10, choices=APPROVAL_CHOICES, default='pending')
     payment_status = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default='unpaid')
     def __str__(self):
